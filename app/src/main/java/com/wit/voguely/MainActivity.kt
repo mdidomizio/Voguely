1bb4849a621b08a0d6bbbc8ff98a434d95a8a389
Miriam <miriam.didomizio@googlemail.com>
2023-01-16 10:58:37 +0100

    finished splash fragment, connected to the login one via navigate

--- app/src/main/java/com/wit/voguely/MainActivity.kt
+++ app/src/main/java/com/wit/voguely/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.wit.voguely
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat.animate
+import com.wit.voguely.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
 }
--- app/src/main/java/com/wit/voguely/SplashFragment.kt
+++ app/src/main/java/com/wit/voguely/SplashFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
+import androidx.navigation.fragment.findNavController
+import com.wit.voguely.databinding.FragmentSplashBinding
 
 
 class SplashFragment : Fragment() {
+
+    private lateinit var binding : FragmentSplashBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +26,19 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        binding = FragmentSplashBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.logo.animate().alpha(1.0F).setDuration(2000).setInterpolator(DecelerateInterpolator())
+            .withEndAction(
+                Runnable {
+                    binding.logo.animate().alpha(0.0F).setDuration(2000)
+                        .withEndAction({findNavController().navigate(R.id.action_splashFragment_to_loginSignupFragment)}).start()
+                }).start()
+    }
 
 }
