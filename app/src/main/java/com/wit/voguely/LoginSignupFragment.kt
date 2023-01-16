9e9cd76c542968add12b7e1459fa4a0f50a7ac49
Miriam <miriam.didomizio@googlemail.com>
2023-01-16 15:21:13 +0100

    added the tabs for sing and login, working, improving the layout

--- app/src/main/java/com/wit/voguely/LoginSignupFragment.kt
+++ app/src/main/java/com/wit/voguely/LoginSignupFragment.kt
@@ -1,10 +1,15 @@
 package com.wit.voguely
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+import com.google.android.material.tabs.TabLayoutMediator
 import com.wit.voguely.databinding.ActivityMainBinding
 import com.wit.voguely.databinding.FragmentLoginSignupBinding
 
@@ -12,6 +17,10 @@ import com.wit.voguely.databinding.FragmentLoginSignupBinding
 class LoginSignupFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginSignupBinding
+
+
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +39,30 @@ class LoginSignupFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
+
+        binding.tabs.addOnTabSelectedListener(object: OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                binding.welcomeBackText.text = getString(R.string.SubtitleTextSignUp)
+                binding.actionButton.text = getString(R.string.signUpButton)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                binding.welcomeBackText.text = getString(R.string.SubtitleTextLogin)
+                binding.actionButton.text = getString(R.string.loginButton)
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                binding.welcomeBackText.text = getString(R.string.SubtitleTextSignUp)
+                binding.actionButton.text = getString(R.string.signUpButton)
+            }
+
+        })
+
+
+        }
+
+
+
 
 
 }
