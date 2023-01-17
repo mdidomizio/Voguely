2c09059d23fd8b85ac1b7ba9381c4ef3b360bfc6
Miriam <miriam.didomizio@googlemail.com>
2023-01-17 10:08:47 +0100

    tab login and signup is now working, layout not yet ready

--- app/src/main/java/com/wit/voguely/LoginSignupFragment.kt
+++ app/src/main/java/com/wit/voguely/LoginSignupFragment.kt
@@ -1,26 +1,19 @@
 package com.wit.voguely
 
-import android.graphics.Color
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
-import com.google.android.material.tabs.TabLayoutMediator
-import com.wit.voguely.databinding.ActivityMainBinding
 import com.wit.voguely.databinding.FragmentLoginSignupBinding
 
 
 class LoginSignupFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginSignupBinding
-
-
-
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +24,7 @@ class LoginSignupFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         binding = FragmentLoginSignupBinding.inflate(inflater, container, false)
         return binding.root
@@ -40,23 +33,33 @@ class LoginSignupFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.tabs.addOnTabSelectedListener(object: OnTabSelectedListener{
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                binding.welcomeBackText.text = getString(R.string.SubtitleTextSignUp)
-                binding.actionButton.text = getString(R.string.signUpButton)
-            }
+        binding.tabs.addOnTabSelectedListener(
+            object : OnTabSelectedListener {
+                override fun onTabSelected(selectedTab: TabLayout.Tab) {
+                    val loginTab = binding.tabs.getTabAt(0)
+                    if(loginTab != null){
+                        if (loginTab.isSelected) {
+                            binding.welcomeBackText.text = getString(R.string.SubtitleTextLogin)
+                            binding.actionButton.text = getString(R.string.loginButton)
+                        } else {
+                            binding.welcomeBackText.text = getString(R.string.SubtitleTextSignUp)
+                            binding.actionButton.text = getString(R.string.signUpButton)
+                        }
+                    }
+
+                }
+
 
             override fun onTabUnselected(tab: TabLayout.Tab?) {
-                binding.welcomeBackText.text = getString(R.string.SubtitleTextLogin)
-                binding.actionButton.text = getString(R.string.loginButton)
+
             }
 
             override fun onTabReselected(tab: TabLayout.Tab?) {
-                binding.welcomeBackText.text = getString(R.string.SubtitleTextSignUp)
-                binding.actionButton.text = getString(R.string.signUpButton)
+
             }
 
-        })
+            }
+        )
 
 
         }
