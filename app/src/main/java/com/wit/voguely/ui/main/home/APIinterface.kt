5f0540de977a21bff1711c2a9f25a7625555ae3c
Miriam <miriam.didomizio@googlemail.com>
2023-01-18 10:25:36 +0100

    added an interface/api and an adapter for the recycler view

---
+++ app/src/main/java/com/wit/voguely/ui/main/home/APIinterface.kt
@@ -0,0 +1,7 @@
+package com.wit.voguely.ui.main.home
+
+interface APIinterface {
+    //TODO @Headers("Authorization: Client-ID CtRI7-qcyLl787SO5Juw5ICvfAwfvZg-pZKhKmo5kYk")
+    //    @GET("photos/random")
+    //    suspend fun getRandomPic(): PhotoGalleryClass
+}
---
+++ app/src/main/java/com/wit/voguely/ui/main/home/HomeAdapter.kt
@@ -0,0 +1,29 @@
+package com.wit.voguely.ui.main.home
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.wit.voguely.databinding.FragmentHomeBinding
+
+class HomeAdapter : RecyclerView.Adapter<HomeAdapter.ViewHolder> () {
+    var data : MutableList<DataItemClass> = mutableListOf()
+
+    inner class ViewHolder(val binding:FragmentHomeBinding) : RecyclerView.ViewHolder(binding.root){
+        init {
+            //TODO => binding.singleItem.setOnClickListener{
+            //                onItemClick?.invoke(data[adapterPosition])
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
+
