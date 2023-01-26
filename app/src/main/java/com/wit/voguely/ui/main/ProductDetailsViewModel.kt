d7028c3accef249af557acf775db10efc23df055
Miriam <miriam.didomizio@googlemail.com>
2023-01-26 10:08:12 +0100

    stated to develop the product details, marging the fetch data to main

---
+++ app/src/main/java/com/wit/voguely/ui/main/ProductDetailsViewModel.kt
@@ -0,0 +1,10 @@
+package com.wit.voguely.ui.main
+
+import androidx.lifecycle.ViewModel
+import com.wit.voguely.remote.ProductsDataSource
+
+class ProductDetailsViewModel : ViewModel() {
+    private val productDetailsViewModel = ProductsDataSource()
+
+    private val
+}
